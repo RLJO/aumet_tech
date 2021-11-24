3b016ef99c711b2e13eabc0eec846deb2cf5dd25
ihab-shhadat <ikshhadat17@gmail.com>
2021-11-25 00:30:19 +0200

    edit

--- flexipharmacy/static/src/js/models.js
+++ flexipharmacy/static/src/js/models.js
@@ -31,6 +31,109 @@ odoo.define('flexipharmacy.models', function (require) {
     models.load_fields('hr.employee', ['rfid_pin']);
     models.load_fields('pos.order', ['note']);
     models.load_fields('pos.order.line', ['line_note']);
+    var DB = require('point_of_sale.DB');
+    DB.include({
+        init: function (options) {
+            this._super.apply(this, arguments);
+            this.uom_by_product = {};
+            this.product_by_tmpl_id = {};
+        },
+
+        get_qty_for_selected_uom: function (uom_obj, real_qty) {
+            var newQty = real_qty;
+            if (uom_obj.uom_type == 'bigger') {
+                newQty = (real_qty / uom_obj.factor_inv);
+            } else if (uom_obj.uom_type == 'smaller') {
+                newQty = (real_qty * uom_obj.factor);
+            } else if (uom_obj.uom_type == 'reference') {
+                newQty = real_qty;
+            }
+            return newQty;
+        },
+
+        get_product_by_tmpl_id: function (tmpl_id) {
+            return this.product_by_tmpl_id[tmpl_id];
+        },
+
+        add_product_by_tmpl_id: function () {
+            var myProducts = this.product_by_id;
+            for (var product_id in myProducts) {
+                var product = myProducts[product_id]
+                this.product_by_tmpl_id[product.product_tmpl_id] = product;
+            }
+        },
+
+        get_uom_by_product: function (id) {
+            if (id in this.uom_by_product) {
+                return this.uom_by_product[id];
+            }
+            return []
+        },
+
+        add_uoms: function (uoms) {
+            var self = this;
+            _.each(uoms, function (unit) {
+                var uom_product = self.get_product_by_tmpl_id(unit.product_id[0]);
+                if (uom_product.id in self.uom_by_product) {
+                    console.log();
+                } else {
+                    var base_uom = {
+                        "id": uom_product.uom_id[0], "price": uom_product.lst_price,
+                        "product_id": [uom_product.id, uom_product.display_name],
+                        "uom_id": uom_product.uom_id
+                    }
+                    self.uom_by_product[uom_product.id] = []
+                    self.uom_by_product[uom_product.id].push(base_uom)
+                }
+                self.uom_by_product[uom_product.id].push(unit);
+
+            });
+
+            var myProducts = this.product_by_id;
+            for (var product_id in myProducts) {
+                var product = myProducts[product_id]
+                if (product_id in self.uom_by_product) {
+                    console.log();
+                } else {
+                    var base_uom = {
+                        "id": product.uom_id[0], "price": product.lst_price,
+                        "product_id": [product.id, product.display_name],
+                        "uom_id": product.uom_id
+                    }
+                    self.uom_by_product[product.id] = []
+                    self.uom_by_product[product.id].push(base_uom);
+                }
+            }
+        },
+
+        get_uoms: function (serial) {
+            return this.get_uom_by_product(serial.product_id[0])
+        }
+    });
+
+    models.load_models({
+        model: 'product.multi.uom.price',
+        fields: ['uom_id', 'product_id', 'price'],
+        loaded: function (self, uomPrice) {
+            self.product_uom_price = {};
+            if (uomPrice.length) {
+                self.db.add_product_by_tmpl_id();
+                self.db.add_uoms(uomPrice);
+                _.each(uomPrice, function (unit) {
+                    if (!self.product_uom_price[unit.product_id[0]]) {
+                        self.product_uom_price[unit.product_id[0]] = {};
+                        self.product_uom_price[unit.product_id[0]].uom_id = {};
+                    }
+                    self.product_uom_price[unit.product_id[0]].uom_id[unit.uom_id[0]] = {
+                        id: unit.uom_id[0],
+                        name: unit.uom_id[1],
+                        price: unit.price,
+                    };
+                });
+            }
+        },
+    });
+
 
     var _super_paymentline = models.Paymentline.prototype;
     var _super_Order = models.Order.prototype;
@@ -94,28 +197,6 @@ odoo.define('flexipharmacy.models', function (require) {
         },
     });
     models.PosModel.prototype.models.push(
-        {
-            model: 'product.multi.uom.price',
-            fields: ['uom_id', 'product_id', 'price'],
-            loaded: function (self, uomPrice) {
-                self.product_uom_price = {};
-                if (uomPrice.length) {
-                    self.db.add_product_by_tmpl_id();
-                    self.db.add_uoms(uomPrice);
-                    _.each(uomPrice, function (unit) {
-                        if (!self.product_uom_price[unit.product_id[0]]) {
-                            self.product_uom_price[unit.product_id[0]] = {};
-                            self.product_uom_price[unit.product_id[0]].uom_id = {};
-                        }
-                        self.product_uom_price[unit.product_id[0]].uom_id[unit.uom_id[0]] = {
-                            id: unit.uom_id[0],
-                            name: unit.uom_id[1],
-                            price: unit.price,
-                        };
-                    });
-                }
-            },
-        },
         {
             model: 'aspl.gift.card.type',
             fields: ['name'],
