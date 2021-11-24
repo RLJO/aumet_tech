e667c45d91982a935386a87bf8d11221190aacf7
ihab-shhadat <ikshhadat17@gmail.com>
2021-11-24 22:26:46 +0200

    edit

--- aumet_pos_uom/static/src/js/PackLotLineScreen/PackLotLineScreen.js
+++ aumet_pos_uom/static/src/js/PackLotLineScreen/PackLotLineScreen.js
@@ -11,6 +11,7 @@ odoo.define('aumet_pod_uom.PackLotLineScreen', function (require) {
                 super(...arguments);
                 // onMounted(this.setFirstOneSelected.bind(this));
             }
+
             // setFirstOneSelected() {
             //     var self=this;
             //     if (this.props.serials.length == 1) {
@@ -40,29 +41,30 @@ odoo.define('aumet_pod_uom.PackLotLineScreen', function (require) {
             //     }
             //     // this.render();
             // };
-            _closePackLotScreen(){
+            _closePackLotScreen() {
                 let serialApplied = [];
                 let orderLine = this.env.pos.get_order().get_selected_orderline();
-                if(this.props.orderline){
+                if (this.props.orderline) {
                     orderLine = this.props.orderline;
                 }
                 var has_lot = false;
-                _.each(orderLine.getPackLotLinesToEdit(), function(packLot){
-                    if(packLot.text != ''){
+                _.each(orderLine.getPackLotLinesToEdit(), function (packLot) {
+                    if (packLot.text != '') {
                         serialApplied.push(packLot.text)
                         has_lot = true;
                     }
                 });
-                _.each(this.props.serials, function(serial){
-                    if(!serialApplied.includes(serial.name)){
+                _.each(this.props.serials, function (serial) {
+                    if (!serialApplied.includes(serial.name)) {
                         serial.isSelected = false;
                     }
                 });
-                if (!has_lot){
+                if (!has_lot) {
                     this.env.pos.get_order().remove_orderline(orderLine);
                 }
                 this.close();
             }
+
             applyPackLotLines() {
                 let orderLine = this.env.pos.get_order().get_selected_orderline();
                 if (this.props.orderline) {
@@ -73,10 +75,11 @@ odoo.define('aumet_pod_uom.PackLotLineScreen', function (require) {
                 if (selectedLines.length >= 1) {
                     let modifiedPackLotLines = {};
                     if (this.props.isSingleItem) {
-                        if (selectedLines[0].inputQty > selectedLines[0].location_product_qty) {
+                        if (selectedLines[0].inputQty > selectedLines[0].product_qty) {
                             alert('Invalid Quantity!');
                             return;
                         } else {
+                            let numberOfInputs = selectedLines[0].inputQty;
                             let newPackLotLines = selectedLines.filter(item => item.id).map(item => ({lot_name: item.name}));
                             orderLine.setPackLotLines({modifiedPackLotLines, newPackLotLines});
                             orderLine.set_quantity(selectedLines[0].inputQty);
--- flexipharmacy/static/src/js/Screens/ProductScreen/OrderWidget.js
+++ flexipharmacy/static/src/js/Screens/ProductScreen/OrderWidget.js
@@ -1,22 +1,23 @@
-odoo.define('pos_serial.OrderWidgetInh', function(require) {
+odoo.define('pos_serial.OrderWidgetInh', function (require) {
     'use strict';
 
     const PosComponent = require('point_of_sale.PosComponent');
     const OrderWidget = require('point_of_sale.OrderWidget');
     const Registries = require('point_of_sale.Registries');
-    const { useRef, useState } = owl.hooks;
-    const { isRpcError } = require('point_of_sale.utils');
+    const {useRef, useState} = owl.hooks;
+    const {isRpcError} = require('point_of_sale.utils');
     var rpc = require('web.rpc');
 
     const OrderWidgetInh = (OrderWidget) =>
         class extends OrderWidget {
-            constructor(){
+            constructor() {
                 super(...arguments);
                 this.state.serials = [];
             }
+
             async _editPackLotLines(event) {
                 var self = this;
-                if(this.env.pos.config.enable_pos_serial){
+                if (this.env.pos.config.enable_pos_serial) {
                     const orderline = event.detail.orderline;
                     const isAllowOnlyOneLot = orderline.product.isAllowOnlyOneLot();
                     const packLotLinesToEdit = orderline.getPackLotLinesToEdit(isAllowOnlyOneLot);
@@ -28,44 +29,48 @@ odoo.define('pos_serial.OrderWidgetInh', function(require) {
                         args: [product_id, product_id.id, picking_type]
                     }
                     try {
-                        await rpc.query(params).then(async function(serials){
-                            if(serials){
-                                 for(var i=0 ; i < serials.length ; i++){
-                                     if(serials[i].remaining_qty > 0){
+                        await rpc.query(params).then(async function (serials) {
+                            if (serials) {
+                                for (var i = 0; i < serials.length; i++) {
+                                    if (serials[i].remaining_qty > 0) {
                                         serials[i]['isSelected'] = false;
                                         serials[i]['inputQty'] = 1;
-                                        if(serials[i].expiration_date){
-                                            let localTime =  moment.utc(serials[i].expiration_date).toDate();
+                                        serials[i]['uom'] = serials[i].product_uom_id[0];
+
+                                        if (serials[i].expiration_date) {
+                                            let localTime = moment.utc(serials[i].expiration_date).toDate();
                                             serials[i]['expiration_date'] = moment(localTime).locale('en').format('YYYY-MM-DD hh:mm A');
                                         }
-                                        if(self.env.pos.config.product_exp_days){
-                                             let product_exp_date = moment().add(self.env.pos.config.product_exp_days, 'd')
-                                                                    .format('YYYY-MM-DD');
-                                             let serial_life = moment(serials[i]['expiration_date']).locale('en').format('YYYY-MM-DD');
-                                             if(product_exp_date >= serial_life){
+                                        if (self.env.pos.config.product_exp_days) {
+                                            let product_exp_date = moment().add(self.env.pos.config.product_exp_days, 'd')
+                                                .format('YYYY-MM-DD');
+                                            let serial_life = moment(serials[i]['expiration_date']).locale('en').format('YYYY-MM-DD');
+                                            if (product_exp_date >= serial_life) {
                                                 serials[i]['NearToExpire'] = 'NearToExpire';
-                                             }
+                                            }
                                         }
                                         self.state.serials.push(serials[i])
-                                     }
-                                 }
-                                self.state.serials.sort(function(a,b){
+                                    }
+                                }
+                                self.state.serials.sort(function (a, b) {
                                     return (b.expiration_date) - (a.expiration_date);
                                 });
-                                self.showScreen('PackLotLineScreen', {isSingleItem : isAllowOnlyOneLot,
-                                                                       orderline : orderline,
-                                                                       serials : self.state.serials});
+                                self.showScreen('PackLotLineScreen', {
+                                    isSingleItem: isAllowOnlyOneLot,
+                                    orderline: orderline,
+                                    serials: self.state.serials
+                                });
                             }
                         });
-                    }catch (error) {
+                    } catch (error) {
                         if (isRpcError(error) && error.message.code < 0) {
                             self.env.pos.get_order().set_connected(false)
                             super._editPackLotLines(event);
                         } else {
                             return;
                         }
-                    } 
-                }else{
+                    }
+                } else {
                     super._editPackLotLines(event);
                 }
             }
