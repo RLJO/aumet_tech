39188d25264b50eb68d526df3bf86bb6c739955f
ihab-shhadat <ikshhadat17@gmail.com>
2021-11-24 23:40:52 +0200

    edit

--- aumet_pos_uom/static/src/js/PackLotLineScreen/PackLotLineScreen.js
+++ aumet_pos_uom/static/src/js/PackLotLineScreen/PackLotLineScreen.js
@@ -11,6 +11,7 @@ odoo.define('aumet_pod_uom.PackLotLineScreen', function (require) {
                 super(...arguments);
                 // onMounted(this.setFirstOneSelected.bind(this));
             }
+
             // setFirstOneSelected() {
             //     var self=this;
             //     if (this.props.serials.length == 1) {
@@ -40,31 +41,35 @@ odoo.define('aumet_pod_uom.PackLotLineScreen', function (require) {
             //     }
             //     // this.render();
             // };
-            _closePackLotScreen(){
+            _closePackLotScreen() {
                 let serialApplied = [];
                 let orderLine = this.env.pos.get_order().get_selected_orderline();
-                if(this.props.orderline){
+                if (this.props.orderline) {
                     orderLine = this.props.orderline;
                 }
                 var has_lot = false;
-                _.each(orderLine.getPackLotLinesToEdit(), function(packLot){
-                    if(packLot.text != ''){
+                _.each(orderLine.getPackLotLinesToEdit(), function (packLot) {
+                    if (packLot.text != '') {
                         serialApplied.push(packLot.text)
                         has_lot = true;
                     }
                 });
-                _.each(this.props.serials, function(serial){
-                    if(!serialApplied.includes(serial.name)){
+                _.each(this.props.serials, function (serial) {
+                    if (!serialApplied.includes(serial.name)) {
                         serial.isSelected = false;
                     }
                 });
-                if (!has_lot){
+                if (!has_lot) {
                     this.env.pos.get_order().remove_orderline(orderLine);
                 }
                 this.close();
             }
+
             applyPackLotLines() {
                 let orderLine = this.env.pos.get_order().get_selected_orderline();
+                var all_order_line = this.env.pos.get_order().orderlines.models
+                var same_order_line
+                // this.env.pos.get_order().orderlines.models[0].pack_lot_lines.models[0].attributes.lot_name
                 if (this.props.orderline) {
                     orderLine = this.props.orderline;
                 }
@@ -73,17 +78,38 @@ odoo.define('aumet_pod_uom.PackLotLineScreen', function (require) {
                 if (selectedLines.length >= 1) {
                     let modifiedPackLotLines = {};
                     if (this.props.isSingleItem) {
-                        if (selectedLines[0].inputQty > selectedLines[0].location_product_qty) {
+                        if (selectedLines[0].inputQty > selectedLines[0].location_product_qty || selectedLines[0].inputQty <= 0) {
                             alert('Invalid Quantity!');
                             return;
                         } else {
-                            let newPackLotLines = selectedLines.filter(item => item.id).map(item => ({lot_name: item.name}));
-                            orderLine.setPackLotLines({modifiedPackLotLines, newPackLotLines});
-                            orderLine.set_quantity(selectedLines[0].inputQty);
-                            orderLine.set_custom_uom_id(parseInt(selectedLines[0].uom));
-                            orderLine.apply_uom();
-                            this._closePackLotScreen();
-                            return;
+                            _.each(all_order_line, function (item) {
+                                if (item.product.id == orderLine.product.id) {
+                                    _.each(item.pack_lot_lines.models, function (lot_id) {
+                                        if (lot_id.attributes.lot_name === selectedLines[0].name) {
+                                            same_order_line = item
+                                        }
+                                    });
+                                }
+                            });
+                            if (same_order_line) {
+                                orderLine.set_quantity('remove')
+                                var total_qty = Number(same_order_line.quantity) + Number(selectedLines[0].inputQty)
+                                if (Number(total_qty) > Number(selectedLines[0].location_product_qty)) {
+                                    alert('Invalid Quantity!');
+                                    return;
+                                }
+                                same_order_line.set_quantity(total_qty)
+                                this._closePackLotScreen();
+                                return;
+                            } else {
+                                let newPackLotLines = selectedLines.filter(item => item.id).map(item => ({lot_name: item.name}));
+                                orderLine.setPackLotLines({modifiedPackLotLines, newPackLotLines});
+                                orderLine.set_quantity(selectedLines[0].inputQty);
+                                orderLine.set_custom_uom_id(parseInt(selectedLines[0].uom));
+                                orderLine.apply_uom();
+                                this._closePackLotScreen();
+                                return;
+                            }
                         }
                     }
                     let newPackLotLines = selectedLines.filter(item => item.id).map(item => ({lot_name: item.name}));
